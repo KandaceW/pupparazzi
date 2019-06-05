903bd652cac5300f11f212c9cb7738728fc7f8a1
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-06-05 15:03:46 +1200

    pupperrrs

--- routes.js
+++ routes.js
@@ -13,4 +13,27 @@ router.get('/',(req, res) =>{
   res.render('puppies/index',viewData)
 })
 
+router.get('/:id',(req, res) =>{
+  const pups = data.puppies.find(item => {
+    return item.id == req.params.id;
+  });
+  const viewData = {
+    puppies: [data.puppies]
+  };
+  console.log(pups);
+  res.render("puppies/view", pups);
+})
+
+router.get('/:id',(req, res) =>{
+  const pups = data.puppies.find(item => {
+    return item.id == req.params.id;
+  });
+  const viewData = {
+    puppies: [data.puppies]
+  };
+  console.log(pups);
+  res.render("puppies/view", pups);
+})
+
+
 module.exports = router

f9c097ac8115864e98827789454d50ba553780df
mikeywipo <49007478+mikeywipo@users.noreply.github.com>
2019-06-05 15:24:09 +1200

    update redirect

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const data = require('../data.json')
+
+
+const viewData = {
+  puppies: data,
+}
 
 router.get('/', (req, res) => {
-  res.send('Pupparazzi')
+  res.redirect('/puppies') // update to redirect to puppies
 })
 
-// router.get('/', (req, res) => {
-//   res.send('This is the home page')
+router.get('/puppies', (req, res) => {
+  res.render('../views/puppies/view')
+})
+
+// server.get('/', (req, res) => {
+//   let id = req.params
+//   res.render('home', viewData)
 // })
 
 // router.get('/', (req, res) => {

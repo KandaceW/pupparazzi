15d10251e40f3d3722ba94026c96dceebfccc84c
abc <abc@example.com>
2019-06-05 12:34:48 +1200

    this is a commit

---
+++ routes.js
@@ -0,0 +1,12 @@
+const express = require ('express')
+const router = express.Router ()
+
+// router.get('/index', (req, res) => {
+//     res.send('Here are the Puppies!')
+// })
+
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 
+const contactRoutes = require('./routes')
+const router = express()
+const puppies = require('./data.json')
+
+
+
 const server = express()
 
 // Middleware
@@ -11,5 +17,10 @@ server.engine('hbs', hbs({
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({extended: false}))
+
+server.get('/', (req, res)=>{
+  res.render('puppies/index', puppies)
+  // res.send('do you hear me?')
+})
 
 module.exports = server

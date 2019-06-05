47027a9b0b9ac82eaf0cb60d93fd256b0bcea005
Reuben Osborne <10952926+reubenosborne@users.noreply.github.com>
2019-06-05 12:45:27 +1200

    second commit-rendering each puppies file -all working

--- routes.js
+++ routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const pups = require("./data.json");
+const puppies = require("./data.json");
 const router = express.Router();
 
 router.get("/puppies", (req, res) => {
-  res.render("puppies/index.hbs", pups);
+  res.render("puppies/index.hbs", puppies);
+});
+
+router.get("/puppies/:id", (req, res) => {
+  const pup = puppies.puppies.find(pup => {
+    return pup.id == req.params.id;
+  });
+  console.log(pup)
+  res.render("puppies/view.hbs", pup);
 });
 
 module.exports = router;

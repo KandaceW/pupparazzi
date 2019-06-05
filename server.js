4be04c9dc9155710cb2f2366cdda72675b739062
Tessa <49006336+tessajane@users.noreply.github.com>
2019-06-05 12:07:23 +1200

    add task

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const hbs = require('express-handlebars')
 
 const server = express()
+const routesFolder = require('./routes')
 
 // Middleware
 server.engine('hbs', hbs({
@@ -11,5 +12,10 @@ server.engine('hbs', hbs({
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({extended: false}))
+
+server.get('/',(req, res) => {
+  res.send('Pupparazzi')
+})
+server.use('/router', routesFolder)
 
 module.exports = server

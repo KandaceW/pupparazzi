08403527bed0107043deb59209e88dde8353aa73
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-06-05 15:24:28 +1200

    pupperrrs

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const data = require('./data.json')
-
+const fs = require('fs')
 
 
 const viewData = {
@@ -39,6 +39,22 @@ router.get('/edit/:id',(req, res) =>{
 // Update the puppy in the array
 // Write the entire array back into the JSON file
 // Redirect to the get /puppies/edit/:id route
+
+router.post('/edit/:id',(req, res) =>{
+//  const pupBeingEdited = {puppies[id]}
+console.log(req.body)
+const pup = data.puppies.find(item => {
+  return item.id == req.params.id;
+});
+//modify pup using req.body then write data back to the file
+pup.name = req.body.name
+pup.breed = req.body.breed
+pup.owner = req.body.owner
+// fs.writeFile
+
+
+console.log(data)
+})
 
 
 module.exports = router

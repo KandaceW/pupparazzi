5af89003d6b6d59f5820c424fcda9b3f89d7d727
Hamish Smith <44103834+hamish-w-smith@users.noreply.github.com>
2019-06-05 15:15:26 +1200

    up to stretch

--- routes/routes.js
+++ routes/routes.js
@@ -1,56 +1,68 @@
-const express = require('express')
-const router = express.Router()
-const puppies = require('../data')
+const express = require("express");
+const router = express.Router();
+const puppies = require("../data");
+const fs = require("fs");
 
-const server = express()
+const server = express();
 
-router.get('/', (req, res) => {
-  res.send('Pupparazzi')
-})
+router.get("/", (req, res) => {
+  res.send("Pupparazzi");
+});
 
 //This route displays puppies on screen
-router.get('/puppies', (req, res) => {
-  res.render("puppies", puppies)
-})
+router.get("/puppies", (req, res) => {
+  res.render("puppies", puppies);
+});
 
 //This route gives information about each puppy
-router.get('/puppies/:id', (req, res) => {
-  
+router.get("/puppies/:id", (req, res) => {
   const puppy = puppies.puppies.find(item => {
-    return item.id == req.params.id
-  })
+    return item.id == req.params.id;
+  });
 
-  res.render('puppies/view', puppy)
+  res.render("puppies/view", puppy);
   // res.send('puppy', puppy)
-})
+});
 
 //get route for update form
-router.get('/puppies/edit/:id', (req, res) => {
-  
+router.get("/puppies/edit/:id", (req, res) => {
   const puppy = puppies.puppies.find(item => {
-    return item.id == req.params.id
-  })
+    return item.id == req.params.id;
+  });
 
-  res.render('puppies/edit', puppy)
-})
+  res.render("puppies/edit", puppy);
+});
 
-router.post('/puppies/edit/:id', (req, res) => {
-  
+router.post("/puppies/edit/:id", (req, res) => {
+  // console.log("THIS IS IN THE BODY OF THE REQUEST ", req.body);
   //get url encoded stuff from request
-  const puppyUpdate = req.
-  
+  let puppyUpdate = req.body; //form data
+
   //locate the puppy we are going to update
-  const puppy = puppies.puppies.find(item => {
-    return item.id == req.params.id
-  })
+  let puppy = puppies.puppies.find(item => {
+    return item.id == req.params.id;
+  });
+  
+  //write the update into the puppy array
+  Object.assign(puppy, puppyUpdate)
 
-  fs.readFile("puppies", (err, data) => {
-    if (err) throw err
-    puppy = 
-  })
+  console.log("NEW VALUE OF PUPPY ", puppy);
+  console.log("UPDATED ARRAY??? ", puppies);
 
-})
+  fs.writeFile("puppies", JSON.stringify(puppies.puppies, null, 2),"utf8", err => {
+      if (err) throw err;
+      console.log('The puppies have been updated');
+      res.render("puppies/edit", puppy);
+    }
+  );
 
+  //read in the puppies.JSON we want to update
+  // fs.readFile("puppies", (err, data) => {
+  //   if (err) throw err
+  //   puppy =
+  // })
 
+  //Write the entire array back into the JSON file
+}); //AFTER THIS WRITE THE REDIRECT FOR THIS ROUTE
 
-module.exports = router
+module.exports = router;
